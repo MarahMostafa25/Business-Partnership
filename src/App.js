e061120854752fe232aa5f4c15cba64f4129a5a2
Marah255 <152785397+MarahMostafa25@users.noreply.github.com>
2023-12-03 16:48:08 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { LoginSignup,LoginSignup1 } from './Components/LoginSignup/LoginSignup';
 function App() {
   return (
     <div>
-            <LoginSignup1/> <LoginSignup/>
+          <LoginSignup1/> <LoginSignup/>
     </div>
   );
 }
