fcb8ba0d36cf6b6aabec79111e50abe7bce926b3
Marah255 <152785397+MarahMostafa25@users.noreply.github.com>
2023-12-03 16:33:12 +0200

    Login-SignUp

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { LoginSignup,LoginSignup1 } from './Components/LoginSignup/LoginSignup';
+
+function App() {
+  return (
+    <div>
+            <LoginSignup1/> <LoginSignup/>
+    </div>
+  );
+}
+
+export default App;
